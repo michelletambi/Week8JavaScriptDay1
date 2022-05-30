77ff6f99157b545020beb2691d979c5a32bc9621
Michelle Tambi <michelletambi22@gmail.com>
2022-05-30 14:43:49 +0100

    completed MVP

--- code/arrays.js
+++ code/arrays.js
@@ -8,8 +8,9 @@ stations = ["Dundee", "Edinburgh", "North Berwick", "Newcastle", "York", "Leeds"
  * Q1.
  * The East Coast Main Line doesn't stop at Peterborough! Add London Kings Cross to the end of the array to complete the line.
  */
+stations.push("London Kings Cross");
 
-// console.log(stations);
+console.log(stations);
 
 
 
@@ -17,19 +18,18 @@ stations = ["Dundee", "Edinburgh", "North Berwick", "Newcastle", "York", "Leeds"
  * Q2.
  * It doesn't go as far as Dundee either, so remove it from the start of the array.
  */
+stations.shift();
 
-// console.log(stations);
-
-
+console.log(stations);
 
 /**
  * Q3.
  * How many stations are on the line?
  */
 
-stationCount = null;
+stationCount = stations.length;
 
-// console.log("There are " + stationCount + " stations.");
+console.log("There are " + stationCount + " stations.");
 
 
 
@@ -38,9 +38,9 @@ stationCount = null;
  * Which station is the third stop?
  */
 
-thirdStation = null;
+thirdStation = stations[2];
 
-// console.log("The third station is " + thirdStation);
+console.log("The third station is " + thirdStation);
 
 
 
@@ -48,8 +48,8 @@ thirdStation = null;
  * Q5.
  * Someone put the wrong Berwick on the timetable! Update "North Berwick" to "Berwick-upon-Tweed".
  */
-
-// console.log(stations);
+stations.splice(1, 1, "Berwick-upon-Tweed");
+console.log(stations);
 
 
 
@@ -57,8 +57,9 @@ thirdStation = null;
  * Q6.
  * How did Leeds get on there? Better remove it.
  */
+stations.splice(4, 1);
 
-// console.log(stations);
+console.log(stations);
 
 
 
@@ -66,8 +67,8 @@ thirdStation = null;
  * Q7.
  * We forgot about Darlington! Add it to the route between Newcastle and York.
  */
-
-// console.log(stations);
+stations.splice(3, 0, "Darlington");
+console.log(stations);
 
 
 
@@ -75,13 +76,24 @@ thirdStation = null;
  * Q8.
  * Is Durham one of the stops?
  */
+containsDurham = false;
+for(i = 0; i < stations.length; i ++){
+    if (stations[i] == "Durham"){
+        containsDurham = true;
+    }
+}
+stoppingAtDurham = containsDurham;
 
-stoppingAtDurham = null;
-
-// console.log(stoppingAtDurham);
+console.log(stoppingAtDurham);
 
 
 /**
  * Q9.
  * If we are stopping at Durham, print "Yay Durham!" to the terminal. If not print "Aww...".
  */
+ for(i = 0; i < stations.length; i ++){
+    if (stations[i] == "Durham"){
+        console.log("Yay Durham!");
+        break;
+    }
+} console.log("Aww...");
--- code/objects.js
+++ code/objects.js
@@ -28,9 +28,9 @@ tortoise = {
  * Q1. What's the name of the dog?
  */
 
-dogName = null;
+dogName = dog.name;
 
-// console.log("The dog's name is " + dogName);
+console.log("The dog's name is " + dogName);
 
 
 
@@ -38,17 +38,18 @@ dogName = null;
  * Q2. What breed is the cat?
  */
 
-catBreed = null;
+catBreed = cat.breed;
 
-// console.log("The cat is a " + catBreed);
+console.log("The cat is a " + catBreed);
 
 
 
 /**
  * Q3. It's the tortoise's birthday! Add one to her current age.
  */
+tortoise.age ++;
 
-// console.log(tortoise)
+console.log(tortoise)
 
 
 
@@ -56,9 +57,9 @@ catBreed = null;
  * Q4. How many different colours is Poppy?
  */
 
-dogColourCount = null;
+dogColourCount = dog.colours.length;
 
-// console.log("The dog has " + dogColourCount + " different colours");
+console.log("The dog has " + dogColourCount + " different colours");
 
 
 
@@ -66,17 +67,23 @@ dogColourCount = null;
  * Q5. I've just bought a 12-year-old African Grey parrot called Lucky. Create an object for her.
  */
 
-parrot = null;
+parrot = {
+    name: "Lucky",
+    age: 12,
+    breed: "African",
+    colours: ["grey"]
+};
 
-// console.log(parrot);
+console.log(parrot);
 
 
 
 /**
  * Q6. Ella's favourite food is dandelion, add this information to her object
  */
+tortoise.favouriteFood = "dandelion";
 
-// console.log(tortoise);
+console.log(tortoise);
 
 
 
@@ -84,14 +91,14 @@ parrot = null;
  * Q7. What's the combined age of all the pets?
  */
 
-totalPetAge = null;
+totalPetAge = dog.age + cat.age + tortoise.age + parrot.age;
 
-// console.log("The total age of the pets is " + totalPetAge);
+console.log("The total age of the pets is " + totalPetAge);
 
 
 
 /**
  * Q8. It's turns out that Morris isn't a European Shorthair. We don't actually know his real breed, so remove that key from his object
  */
-
-// console.log(cat);
+delete cat.breed;
+console.log(cat);
